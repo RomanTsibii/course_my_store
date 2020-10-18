377ca88c3dce8d2389cc5e0fde3b91fffb337a45
RomanTsibii <romanot70@gmail.com>
2020-10-18 21:34:43 +0300

    create model BlogPost and Comment, create polymorphic accosiation with commentable to Item and BlogPost

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,6 @@ class Item < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }, allow_nil: true
   has_many :positions
   has_many :carts, :through => :positions
+  has_many :comments, as: :commentable
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
 
   has_one :cart
   has_many :orders
+  has_many :comments
 
 end
