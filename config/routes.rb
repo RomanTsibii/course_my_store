bd628fdf283ad67825087b9c45e586293fc28204
RomanTsibii <romanot70@gmail.com>
2020-10-19 10:28:49 +0300

    add gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
+  # get 'pages/home'
+  root 'pages#home'
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items do
     get :upvote,    on: :member
     get :expensive, on: :collection
   end
+
+
+  devise_for :users, path: 'auth', path_names:
+      {
+          sign_in: 'login',
+          sign_out: 'logout',
+          password: 'secret',
+          confirmation: 'verification',
+          unlock: 'unblock',
+          registration: 'register',
+          sign_up: 'cmon_let_me_in'
+      }
 end
